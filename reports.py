a829a922e3fb16a4a1a23f8ffc86fb131edda676
Richard Szombat <szombatrichard@gmail.com>
2017-07-05 03:06:07 +0200

    done

--- reports.py
+++ reports.py
@@ -1,20 +1,33 @@
 import csv
-# Report functions
 
+
+def load_list(file_name, index):
+    properties = []
+    reader = open(file_name, "r")
+    game_stats = csv.reader(reader, delimiter="\t")
+    for row in game_stats:
+        properties.append(row[index])
+    reader.close()
+    return properties
+
+
+def sort_list(list_name):
+    for item in range(len(list_name) - 1, 0, -1):
+        for i in range(item):
+            if list_name[i].upper() > list_name[i + 1].upper():
+                list_name[i], list_name[i + 1] = list_name[i + 1], list_name[i]
+    return list_name
 # 1
 
 
 def count_games(file_name):
-    with open(file_name, "r") as game_stats:
-        number = 0
-        for line in game_stats:
-            number += 1
-        return number
+    games = load_list(file_name, 1)
+    return len(games)
 # 2
 
 
 def decide(file_name, year):
-    years = load_list(stat, 2)
+    years = load_list(file_name, 2)
     for game in years:
         if str(game) == str(year):
             return True
@@ -23,8 +36,8 @@ def decide(file_name, year):
 
 
 def get_latest(file_name):
-    titles = load_list(stat, 0)
-    years = load_list(stat, 2)
+    titles = load_list(file_name, 0)
+    years = load_list(file_name, 2)
     latest_game_index = int(years.index(max(years)))
     return titles[latest_game_index]
 
@@ -33,14 +46,14 @@ def get_latest(file_name):
 
 def count_by_genre(file_name, genre):
     import operator
-    genres = load_list(stat, 3)
+    genres = load_list(file_name, 3)
     number_of_games = genres.count(genre)
     return number_of_games
 # 5
 
 
 def get_line_number_by_title(file_name, title):
-    titles = load_list(stat, 0)
+    titles = load_list(file_name, 0)
     while True:
         try:
             line_number = titles.index(title) + 1
@@ -51,33 +64,16 @@ def get_line_number_by_title(file_name, title):
 
 
 def sort_abc(file_name):
-    titles = load_list(stat, 0)
+    titles = load_list(file_name, 0)
     return sort_list(titles)
 # 7
 
 
 def get_genres(file_name):
-    genres = load_list(stat, 3)
+    genres = load_list(file_name, 3)
     sorted_genre = list(set(genres))
     return sort_list(sorted_genre)
-
-
-def load_list(file_name, index):
-    list_type = []
-    reader = open(file_name, "r")
-    target_reader = csv.reader(reader, delimiter="\t")
-    for row in target_reader:
-        list_type.append(row[index])
-    reader.close()
-    return list_type
-
-
-def sort_list(list_name):
-    for item in range(len(list_name) - 1, 0, -1):
-        for i in range(item):
-            if list_name[i].upper() > list_name[i + 1].upper():
-                list_name[i], list_name[i + 1] = list_name[i + 1], list_name[i]
-    return list_name
+# 8
 
 
 def when_was_top_sold_fps(file_name):
@@ -98,14 +94,3 @@ def when_was_top_sold_fps(file_name):
             break
         finally:
             data.close()
-
-
-stat = "game_stat.txt"
-count_games(stat)
-decide(stat, 1997)
-get_latest(stat)
-count_by_genre(stat, "First-person shooter")
-get_line_number_by_title(stat, "World of Warcraft")
-sort_abc(stat)
-get_genres(stat)
-when_was_top_sold_fps(stat)
